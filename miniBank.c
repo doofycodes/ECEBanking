c49c1d4f8656f6f02981e41aa1360681d3e84a4f
doofycodes <feb3adolfo@gmail.com>
2026-04-14 16:06:03 -0600

    Add transfer functionality and update menu options

--- miniBank.c
+++ miniBank.c
@@ -9,10 +9,11 @@ struct account // this creates the structure of an account :)
 };
 
 //prototypes for functions
-void menu(double *pBalance, FILE *pF);
+void menu(double *pBalance, FILE *pF, struct account bankUsers[], int totalUsers, int userIdx);
 void depo(double *pBalance, FILE *pF);
 void with(double *pBalance, FILE *pF);
 void printBalance(double *pBalance);
+void transfer(struct account bankUsers[], int totalUsers, int userIdx, FILE*pf);
 int auth(struct account bankUsers[], int totalUsers);
 
 int main()
@@ -38,12 +39,18 @@ int main()
 
     double *pBalance = &bankUsers[userIdx].balance;
 
-    FILE *pF;
-    pF = fopen("Statement.txt","w");
-    fprintf(pF, "-----Welcome to your statement-----\n\n");
+    /*    fopen(filename, mode);
+                MODES 
+        w - Writes to a file 
+        a - Appends new data to a file | adds to a new line at the end of the file
+        r - Reads from a file
+    */
+    FILE *pF;//this creates a file and assigns it to pointer *pF 
+    pF = fopen("Statement.txt","w");// this opens the Statement.txt file and writes the next line into it 
+    fprintf(pF, "-----Welcome to your statement-----\n\n");// fprintf will print to the file instead of the terminal.  
 
     printf("\n\nHello welcome to Gbank\n");
-    menu(pBalance,pF);//passing the POINTER to menu function 
+    menu(pBalance, pF, bankUsers, totalUsers, userIdx);//passing the POINTER to menu function 
     // when passing a pointer, you wont include the * because you are passing the actual address. When you receive a pointer, you must specify with a 
     //* because you are saying that you are passing a POINTER
     // you can use/change the actual value of the pointer by DEREFERENCING by putting a * before the name
@@ -53,18 +60,16 @@ int main()
 }
 
 //this function will determine what the user wants to do and then call the next appropriate function 
-void menu(double *pBalance, FILE *pF)
+void menu(double *pBalance, FILE *pF, struct account bankUsers[], int totalUsers, int userIdx)
 {
-
     int choice;
-
-    while(choice !=4){
-
+    while(choice !=5){
     //print options
     printf("1. Deposit money\n");
     printf("2. Withdraw money\n");
     printf("3. Check Balance\n");
-    printf("4. Exit\n\n");
+    printf("4. Transfer Money\n");
+    printf("5. Exit\n\n");
     printf("Enter your choice:");
     
     //take and store choice
@@ -72,7 +77,6 @@ void menu(double *pBalance, FILE *pF)
 
     //call functions 
     switch(choice){
-
         case 1:
         depo(pBalance,pF);
         break;
@@ -86,6 +90,10 @@ void menu(double *pBalance, FILE *pF)
         break;
 
         case 4:
+        transfer(bankUsers, totalUsers, userIdx,pF);
+        break;
+
+        case 5:
         printf("thank you!\n");
         break;
 
@@ -95,57 +103,54 @@ void menu(double *pBalance, FILE *pF)
         }
     }
 }
-
 void depo(double *pBalance, FILE *pF)
 {
     //declare variable and store deposit amount
     double amount;
-    printf("Enter deposit amount: ");
+    printf("Enter deposit amount: $");
     scanf("%lf", &amount);
 
     //make sure deposit amount is positive
     if(amount <0){ 
-        printf("\ninvalid amount, please try again\nEnter deposit amount: ");
+        printf("\ninvalid amount, please try again\nEnter deposit amount: $");
         scanf("%lf", &amount);
     }
 
     //add deposit amount to balance
     *pBalance += amount;//dereference
 
-    //send the deposit and new balance to history file
-    fprintf(pF, "Deposited %.2lf\n", amount);
-    fprintf(pF, "   Balance: %.2lf\n", *pBalance);
+    //send the deposit and new balance to statement.txt file
+    fprintf(pF, "Deposited $%.2lf\n", amount);
+    fprintf(pF, "   Balance: $%.2lf\n", *pBalance);
     //print success message and new balance to terminal
-    printf("\nDeposit succesful\nNew balance: %.2lf\n\n",*pBalance);
+    printf("\nDeposit succesful\nNew balance: $%.2lf\n\n",*pBalance);
 }
- 
 void with(double *pBalance, FILE *pF)
 {
-
     //declare variable and store withdrawal amount
     double amount;
-    printf("please enter withdrawal amount\n");
+    printf("please enter withdrawal amount: $");
     scanf("%lf", &amount);
 
     //make sure withdrawal is less than balance
     while(amount > *pBalance){ 
-        printf("\ninsufficient funds. \nEnter a new withdraw amount: ");
+        printf("\ninsufficient funds.\nEnter a new withdraw amount: $");
         scanf("%lf", &amount);
     }
 
     //this will check if the withdraw amount is divisible by 20. fmod(numerator,denominator)
     while(fmod(amount, 20.0) !=0){
-        printf("\nYou can only withdraw in multiples of 20. \nEnter a new withdraw amount: ");
+        printf("\nYou can only withdraw in multiples of $20. \nEnter a new withdraw amount: $");
         scanf("%lf", &amount);
     }
     //withdraw money from balance
     *pBalance -= amount;
 
-    //send the deposit and new balance to history file
-    fprintf(pF, "Withdrew %.2lf\n", amount);
-    fprintf(pF, "   Balance: %.2lf\n", *pBalance);
+    //send the deposit and new balance to Statement.txt file
+    fprintf(pF, "Withdrew $%.2lf\n", amount);
+    fprintf(pF, "   Balance: $%.2lf\n", *pBalance);
     //print success message and new balance to terminal
-    printf("\nWithdrawal successful\nNew balance %.2lf \n\n", *pBalance);
+    printf("\nWithdrawal successful\nNew balance $%.2lf \n\n", *pBalance);
 
 }
 void printBalance(double *pBalance)
@@ -176,14 +181,46 @@ int auth(struct account bankUsers[], int totalUsers)
                 {
                     return i;
                 }
-
                 printf("incorrect PIN, please try again \n");
                 scanf("%d", &tempPIN);
-                
-
             }
         }    
     }
     return -1;
-    
+}
+void transfer(struct account bankUsers[], int totalUsers, int userIdx,FILE *pF){
+    // ask where the money is going
+    //take money from current users account 
+    // delete from current account and give to new account
+
+    int tempAccount;
+    double tAmount=0.0;
+    printf("\n\n----------TRANSFERING----------\n");
+    printf("Please enter the account number you want to transfer to: ");
+    scanf("%d",&tempAccount);
+
+
+    for(int i=0;  i < totalUsers; i++)
+    {
+        if(tempAccount == bankUsers[i].id)
+        {
+            //find account and collect transfer amount
+            printf("Account %d found!\n", bankUsers[i].id);
+            printf("Please enter the amount you wish to transfer: $");
+            scanf("%lf", &tAmount);
+
+            //transfer logic
+            bankUsers[userIdx].balance -= tAmount;
+            bankUsers[i].balance += tAmount;
+
+            //success message
+            printf("\n-----Transfer complete!-----\n");
+            printf("Your new balance: $%.2lf\n", bankUsers[userIdx].balance);
+            printf("Account %d balance: $%.2lf\n\n", bankUsers[i].id, bankUsers[i].balance);
+
+            //send details to Statement.txt
+            fprintf(pF, "Transfered $%.2lf to account %d\n", tAmount, bankUsers[i].id);
+            fprintf(pF, "   Balance $%.2lf\n",bankUsers[userIdx].balance);
+        }   
+    }
 }
